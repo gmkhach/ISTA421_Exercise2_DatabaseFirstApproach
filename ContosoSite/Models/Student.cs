d81e3f845996c5c5e13efe38e8920f6b29e93cae
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-27 20:03:13 -0400

    Data annotation is added to restrict input lengths and ranges

--- ContosoSite/Models/Student.cs
+++ ContosoSite/Models/Student.cs
@@ -11,22 +11,24 @@ namespace ContosoSite.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Student
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Student()
         {
             this.Enrollments = new HashSet<Enrollment>();
         }
     
         public int StudentID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string FirstName { get; set; }
         public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        [StringLength(50)]
         public string MiddleName { get; set; }
     
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
